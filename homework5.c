4088ff7aa79200d76cec4538f653a880a3ead165
dougporreca <dporreca@syr.edu>
2017-03-02 13:28:59 -0500

    added usage information

--- homework5.c
+++ homework5.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
 	bool opt_h = false, opt_d = false;
 	int opt = getopt(argc, argv, "hd");
-	while (opt != -1)
+	while (opt != -1) //GetOpts
 	{
 		if (opt == 'h') {
 			opt_h = true;
@@ -16,11 +16,20 @@ int main(int argc, char *argv[]) {
 		opt = getopt(argc, argv, "hd");
 	}
 
-	if (opt_h) {
+	
+	if (opt_h) //Print Help Information
+	{
 		printf("*Display Help Information*\n");
 	}
-	if (opt_d) {
-		printf("you selected option d\n");
+
+	if (opt_d) //Customize Backup Location
+	{
+		printf("*Customize Backup Location*\n");
+	}
+
+	if (argc < 2) //Print Usage Information
+	{
+		printf("Usage: %s EXECUTABLE\n",argv[0]);
 	}
 
 	return EXIT_SUCCESS;
