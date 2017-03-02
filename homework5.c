d53f709ce2e1ee039a278127ac879cd51f71edc9
dougporreca <dporreca@syr.edu>
2017-03-02 13:15:46 -0500

    added getopt

--- homework5.c
+++ homework5.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-	
+int main(int argc, char *argv[]) {
+	bool opt_h = false, opt_d = false;
+	int opt = getopt(argc, argv, "hd");
+	while (opt != -1)
+	{
+		if (opt == 'h') {
+			opt_h = true;
+		} else if (opt == 'd') {
+			opt_d = true;
+		}
+		opt = getopt(argc, argv, "hd");
+	}
+
+	if (opt_h) {
+		printf("*Display Help Information*\n");
+	}
+	if (opt_d) {
+		printf("you selected option d\n");
+	}
+
 	return EXIT_SUCCESS;
-	//test boolean
 }
